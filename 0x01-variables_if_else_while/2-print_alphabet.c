76dc03891bdd5f24c8cbd75fbf4a523a141a8319
koblan1 <kelechukwu9@gmail.com>
2022-12-10 10:23:47 -0800

    adding aplhabets in small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - mainrints the alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
