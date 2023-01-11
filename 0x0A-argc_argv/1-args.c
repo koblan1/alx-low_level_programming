50590c19d598e20519580c518024cbb63a1dbe85
koblan1 <kelechukwu9@gmail.com>
2023-01-11 08:53:44 -0800

    redo task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,10 @@
  * Return: 0 - success
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
+	(void) argv /*ignore argv*/
 	printf("%d\n", argc - 1);
+
 	return (0);
 }
